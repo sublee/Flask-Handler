766f4827abfdf9298768fa74098cfdf12b477b16
Kijun Seo <kijun@kijunseo.com>
2011-07-15 15:50:02 +0900

    initial commit

---
+++ flaskext/handler.py
@@ -0,0 +1,175 @@
+import flask
+import werkzeug.exceptions as error
+
+
+__all__ = ['BaseHandler', 'add_handler']
+
+
+POSSIBLE_METHODS = "GET", "POST", "PUT", "DELETE", "HEAD"  
+
+
+def _jinja_renderer(path):
+    def render_from_dict(context_dict):
+        return render_template(path, **context_dict)
+    return render_from_dict
+
+
+class BaseHandler(object):
+    """An inheritable handler inspired by ``the webapp framework``_
+
+    Here's a simple example:
+
+    .. sourcecode:: pycon
+
+       >>> from flaskext.handler import BaseHandler
+       >>> class Hello(BaseHandler):
+       ...     RULE = '/'
+       ...
+       ...     def get(self):
+       ...         return 'hello world'
+
+    A more complicated example:
+
+    .. sourcecode:: pycon
+
+       >>> # some imports
+       >>> from flask import Flask
+       >>> from flaskext.handler import BaseHandler, add_handler
+       >>> from werkzeug.exceptions import NotFound
+       >>> # handler definition
+       >>> class AuthenticatedUser(BaseHandler):
+       ...     RULE = '/me'
+       ...     TEMPLATE_NAME = 'profile'
+       ...
+       ...     def validate_get_request(self, req): # called first
+       ...         token = req.args.get('access_token')
+       ...         user = user_from_access_token(token)
+       ...         if user is None:
+       ...              raise NotFound('...')
+       ...         return user
+       ...
+       ...     def get(self, user): # called after validation
+       ...         return dict(id=user.id, name=user.name, email=user.email)
+       >>>
+       >>> app = Flask("__name__")
+       >>> add_handler(app, AuthenticatedUser)
+
+    This configuration guarantees that ``AuthenticatedUser.get`` will always
+    receive a user object. When you request `/me` with `format=json`, it will
+    render the dict in json.
+    
+    What's really nice about this configuration is that you can easily
+    inherit validations:
+
+    .. sourcecode:: pycon
+
+       >>> class SignedUserFriends(SignedUser):
+       ...     RULE = '/me/friends'
+       ...
+       ...     def get(self, user):
+       ...         return dict(user=user, friends=user.friends)
+
+    Take a look at :meth:`BaseHandler.__call__` to learn how it actually works.
+
+    .. seealso:: :meth:`BaseHandler.__call__`
+
+    """
+    #: The RULE this handler should bind to.
+    RULE = None
+
+    #: The name of the template
+    TEMPLATE_NAME = None
+
+    #: Supported formats
+    DEFAULT_FORMAT = "html"
+
+    #: The name of the form parameter used to indicate the overridden
+    #: method of the request. Set it to None if you want to disable method hack.
+    METHOD_HACK = "__method__"
+
+    # customized renderers
+    CUSTOM_RENDERER = {"json": flask.jsonify}
+
+    def __init__(self):
+        if self.RULE is None:
+            raise NotImplementedError("A BaseHandler implementation must "
+                                      "include self.RULE")
+        # do this inside a metaclass?
+        self.METHODS = [meth for meth in POSSIBLE_METHODS if
+                        hasattr(self, meth.lower())]
+
+    def __call__(self, **kwargs):
+        """Flask will execute this method directly.
+
+        When Flask receives a request, it invokes ``__call__`` method of the
+        registered handler with the appropriate arguments. ``BaseHandler.__call__``
+        processes requests in the following order:
+
+          1. finds the appropriate view form 
+          2. resolves the REST verb.
+          3. validates request using the method associated with the REST verb.
+          4. calls the appropriate method with the validated parameters.
+          5. renders the output using the template format specified.
+
+        This method runs inside `with`, which means that you can override
+        `__enter__` and `__exit__` of your handler and get events to happen
+        at setup and teardown.
+
+        """
+        with self:
+            renderer = self._find_renderer()
+            verb = self._resolve_rest_verb().lower()
+            validator = getattr(self, "validate_%s_request" % verb, None)
+            method = getattr(self, verb)
+
+            if callable(validator):
+                params = validator(flask.request, **kwargs)
+            else:
+                params = kwargs
+
+            if isinstance(params, (list, tuple)):
+                context = method(*params)
+            else:
+                context = method(params)
+
+            if renderer:
+                return renderer(context)
+            return context
+
+    def __enter__(self):
+        """Override this method to run code at setup."""
+        pass
+
+    def __exit__(self, type, value, traceback):
+        """Override this method to run code at teardown."""
+        pass
+
+    def _find_renderer(self):
+        format = flask.request.values.get("format")
+        if format is None:
+            format = self.DEFAULT_FORMAT
+        if format in self.CUSTOM_RENDERER:
+            return self.CUSTOM_RENDERER[format]
+        if self.TEMPLATE_NAME:
+            return _jinja_renderer(self.TEMPLATE_NAME + format)
+        return None
+
+    def _resolve_rest_verb(self):
+        method = flask.request.method
+        if method == "POST" and self.METHOD_HACK:
+            fake_meth = flask.request.form.get(self.METHOD_HACK)
+            if fake_meth:
+                method = fake_meth
+        if method.upper() not in self.METHODS:
+            raise error.MethodNotAllowed("% is an invalid method" % meth)
+        return method
+
+
+def add_handler(app, *handlers):
+    for handler_cls in handlers:
+        handler = handler_cls()
+        app.add_url_rule(handler.RULE,
+                         handler.__class__.__name__,
+                         handler,
+                         methods=handler.METHODS)
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+Flask-Handler
+-------------
+
+Inheritable request handlers for Flask (Webapp inspired)
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-Handler>`_
+* `development version
+  <http://github.com/kijun/flask-handler/zipball/master#egg=Flask-Handler-dev>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Handler',
+    version='0.1',
+    url='http://github.com/kijun/flask-handler'
+    license='BSD',
+    author='Kijun Seo',
+    author_email='m@kijun.co',
+    description='Inheritable request handlers for Flask (Webapp inspired)',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
